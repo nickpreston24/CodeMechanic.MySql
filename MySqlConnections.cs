4fb4d064bc333e80d30c77eab9b4b8b5998aa246
nickpreston24 <nickpreston24@gmail.com>
2025-04-03 10:33:11 -0400

    updated packages

--- MySqlConnections.cs
+++ MySqlConnections.cs
@@ -3,22 +3,22 @@ using MySql.Data.MySqlClient;
 
 namespace CodeMechanic.MySql;
 
-public static class SqlConnections
+public static class SQLConnections
 {
-    public static MySqlConnection Create() =>
-        new MySqlConnection(
-            GetMySQLConnectionString());
+    public static MySqlConnection CreateConnection() => GetMySQLConnectionString().AsConnection();
+
+    public static MySqlConnection AsConnection(this string connectionString) =>
+        new MySqlConnection(connectionString);
 
     public static string GetMySQLConnectionString()
     {
-        var connectionString = new MySqlConnectionStringBuilder
+        var connectionString = new MySqlConnectionStringBuilder()
         {
             Database = Environment.GetEnvironmentVariable("MYSQLDATABASE"),
             Server = Environment.GetEnvironmentVariable("MYSQLHOST"),
             Password = Environment.GetEnvironmentVariable("MYSQLPASSWORD"),
             UserID = Environment.GetEnvironmentVariable("MYSQLUSER"),
-            Port = (uint)Environment.GetEnvironmentVariable("MYSQLPORT")
-                .ToInt(),
+            Port = (uint)Environment.GetEnvironmentVariable("MYSQLPORT").ToInt(),
         }.ToString();
 
         if (connectionString.IsEmpty())
